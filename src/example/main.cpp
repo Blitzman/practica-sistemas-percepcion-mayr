96b4f419e4396b8d96fd45905e594e8a4a3c54f4
Albert Garcia <albert.garcia.ua@gmail.com>
2016-01-21 23:14:57 +0100

    add image processing to main

--- src/example/main.cpp
+++ src/example/main.cpp
@@ -6,6 +6,7 @@
 int main(int argc, char** argv)
 {
 	ImageShapeSegmentation iss_(1, "../../../resources/Video_2/output-%04d.jpg");
+	//ImageShapeSegmentation iss_(0, "../../../resources/Picture_21.jpg");
 
 	cv::Mat frame_;
 	cv::Size size_(640, 480);
